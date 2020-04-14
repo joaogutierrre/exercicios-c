b96a01a43fbd1276817be815b26429cd6d8cc739
João Gutierre <gutierrejoao@gmail.com>
2020-04-14 23:29:46 +0000

    Exercício finalizado e pronto para a entrega.

--- prova/ex03prova.c
+++ prova/ex03prova.c
@@ -5,18 +5,26 @@ int main(){
 
     int resto1 = n % 3;
     int resto2 = (n / 3) % 3;
-    
-    if(resto1 == 0 && resto2 == 0){
-        int resto3 = ((n/3)%3) / 3;
-        printf("%d\n",resto1);
-        printf("%d\n",resto2);
-        printf("%d\n",resto3);
-        printf("Resposta final: %d%d%d\n",resto3,resto2,resto1);
+
+    // depois do 27 as divisões começam a ter mais de 4 algarismos, quebrando a regra do enunciado de manter até 3 algarismos
+    if(n < 27){
+        if(n >= 9){
+            //depois do 9 as divisões começam a ter três algarismos
+            int resto3 = ((n/3)/3) % 3;
+            printf("%d\n",resto1);
+            printf("%d\n",resto2);
+            printf("%d\n",resto3);
+            printf("Resposta final: %d%d%d\n",resto3,resto2,resto1);
+        }else{
+            printf("%d\n",resto1);
+            printf("%d\n",resto2);
+            printf("Resposta final: %d%d\n",resto2,resto1);
+        }
     }else{
-        printf("%d\n",resto1);
-        printf("%d\n",resto2);
-        printf("Resposta final: %d%d\n",resto2,resto1);
+        printf("Conversão com resultado contém mais de 4 algarismos, tente novamente\n");
     }
     
-
+        
+    
+    return 0;
 }
